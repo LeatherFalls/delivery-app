e812ca6a59a29de2f5c12febfacee90b8b22cd53
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-26 11:02:10 -0300

    feat(frontend): removendo o local storage quando dar logout

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import search from '../../assets/images/search.svg';
 import menu from '../../assets/images/menu.svg';
 
 export default function Header() {
-  const teste = JSON.parse(localStorage.getItem('user'));
-  console.log(teste);
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('user'));
+  const logout = () => {
+    localStorage.removeItem('user');
+    navigate('/login');
+  };
   return (
     <header className="header-container">
       <div className="search-and-profile">
-        <img src={ menu } alt="avatar" />
+        <input
+          type="image"
+          src={ menu }
+          alt="avatar"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ logout }
+        />
         <h5
           data-testid="customer_products__element-navbar-user-full-name"
         >
-          {teste.name}
+          {user.name}
         </h5>
         <img src={ search } alt="search" />
       </div>
