128279f689c20e36dd500b2c13f91dc1d16ab460
phvleite <phvleite@gmail.com>
2022-09-27 19:56:26 -0300

    rotas para pegar vendas por usuário e vendedor

--- back-end/src/api/routes/sales.route.js
+++ back-end/src/api/routes/sales.route.js
@@ -7,7 +7,9 @@ const routeSale = '/sales';
 
 saleRouter.post(routeSale, saleController.create);
 saleRouter.get(routeSale, saleController.getAll);
-saleRouter.get(`${routeSale}/:id`, saleController.getById);
+saleRouter.get(`${routeSale}/seller/:id`, saleController.getBySellerId);
+saleRouter.get(`${routeSale}/user/:id`, saleController.getByUserId);
+saleRouter.get(`${routeSale}/:id`, saleController.getBySaleId);
 saleRouter.put(`${routeSale}/:id`, saleController.update);
 saleRouter.delete(`${routeSale}/:id`, saleController.delete);
 
