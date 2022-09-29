bec0663d81c8308aaadc25343baca6273b8356f0
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-29 17:36:09 -0300

    feat(frontend): adicionando as rotas de seller

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -7,6 +7,8 @@ import Products from './pages/Products';
 import Orders from './pages/Orders';
 import SignUp from './pages/SignUp';
 import OrdersDetails from './pages/OrdersDetails';
+import Seller from './pages/Seller';
+import SellerDetails from './pages/SellerDetails';
 
 function AppRoutes() {
   return (
@@ -29,6 +31,15 @@ function AppRoutes() {
       />
       <Route path="/customer/orders" element={ <Orders /> } />
       <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route
+        path="/login/seller/orders"
+        element={ <Navigate to="/seller/orders" /> }
+      />
+      <Route path="/seller/orders" element={ <Seller /> } />
+      <Route
+        path="/seller/orders/:id"
+        element={ <SellerDetails /> }
+      />
       <Route path="/admin/manage" element={ <Admin /> } />
     </Routes>
   );
