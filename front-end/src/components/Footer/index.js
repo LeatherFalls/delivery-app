23e7e8b72673973a24cbe40c0933ce370a92b3c1
Thiago Medeiros <medeiroshs@hotmail.com>
2022-09-29 17:39:28 -0300

    feat(frontend): colocando o footer dinâmico

--- front-end/src/components/Footer/index.js
+++ front-end/src/components/Footer/index.js
@@ -10,64 +10,65 @@ import './styles.css';
 export default function NavBar() {
   const navigate = useNavigate();
   const { sumIsLife } = useContext(globalContext);
+  const user = JSON.parse(localStorage.getItem('user'));
   // const [disabled, setDisabled] = useState(false);
+
+  const ordersPath = () => {
+    if (user.role === 'seller') navigate('/seller/orders');
+    if (user.role === 'customer') navigate('/customer/orders');
+  };
 
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <input
-          type="image"
-          src={ home }
-          alt="Home"
-          data-testid="customer_products__element-navbar-link-products"
-          onClick={ () => navigate('/customer/products') }
-        />
-        <input
-          type="image"
-          src={ order }
-          alt="Order"
-          data-testid="customer_products__element-navbar-link-orders"
-          onClick={ () => navigate('/customer/orders') }
-        />
-        <button
-          type="submit"
-          data-testid="customer_products__button-cart"
-          onClick={ () => navigate('/customer/checkout') }
-          disabled={ Number(sumIsLife) === 0 }
-          style={ { width: '5%' } }
-        >
-          <img src={ cart } alt="cart" />
-          <p data-testid="customer_products__checkout-bottom-value">
-            {`R$ ${String(sumIsLife).replace('.', ',')}`}
-          </p>
-        </button>
-        <input
-          type="image"
-          src={ profile }
-          alt="Profile"
-          onClick={ () => navigate('/customer/profile') }
-        />
-        {/* {
-          disabled
-            && (
+        {
+          user.role === 'seller'
+            ? (
+              <input
+                type="image"
+                src={ order }
+                alt="Order"
+                data-testid="customer_products__element-navbar-link-orders"
+                onClick={ ordersPath }
+              />
+            )
+            : (
               <>
                 <input
                   type="image"
-                  // src={ profile }
-                  alt="Profile"
-                  data-testid="customer_products__element-navbar-link-logout"
-                  onClick={ () => navigate('/customer/profile') }
+                  src={ home }
+                  alt="Home"
+                  data-testid="customer_products__element-navbar-link-products"
+                  onClick={ () => navigate('/customer/products') }
                 />
                 <input
                   type="image"
-                  // src={ profile }
+                  src={ order }
+                  alt="Order"
+                  data-testid="customer_products__element-navbar-link-orders"
+                  onClick={ ordersPath }
+                />
+                <button
+                  type="submit"
+                  data-testid="customer_products__button-cart"
+                  onClick={ () => navigate('/customer/checkout') }
+                  disabled={ Number(sumIsLife) === 0 }
+                  style={ { width: '5%' } }
+                >
+                  <img src={ cart } alt="cart" />
+                  <p data-testid="customer_products__checkout-bottom-value">
+                    {`R$ ${String(sumIsLife).replace('.', ',')}`}
+                  </p>
+                </button>
+                <input
+                  type="image"
+                  src={ profile }
                   alt="Profile"
-                  data-testid="customer_products__element-navbar-link-logout"
-                  onClick={ () => navigate('/login') }
+                  onClick={ () => navigate('/customer/profile') }
                 />
               </>
             )
-        } */}
+        }
       </div>
     </footer>
   );
